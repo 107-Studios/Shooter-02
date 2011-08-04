5e01f90dbf468c9d32868da9e079eed5b87cdde5
ToxicAnt <ghostface1990@gmail.com>
2011-08-03 21:44:29 -0400

    added GameObject shell

---
+++ shooter02/shooter02/GameObjects/GameObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace shooter02.GameObjects
+{
+    class CGameObject
+    {
+        protected Vector2 m_vPosition;
+        protected Vector2 m_vDirection;
+
+        protected Texture m_texImage;
+
+        public void Update(float fTimeElapsed)
+        {
+
+        }
+
+        public void Render()
+        {
+            
+        }
+    }
+}
