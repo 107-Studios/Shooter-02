288c136c39affdb60675fedd1fb5eb0d53d72d35
Brian Kelsey <brian.kelsey2@gmail.com>
2011-10-12 20:29:46 -0400

    Created ObjectManager. Handles adding, removing, and updating of objects.

---
+++ shooter02/shooter02/ObjectManager/ObjectManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using shooter02.GameObjects;
+
+namespace shooter02.ObjectManager
+{
+    class CObjectManager
+    {
+        static readonly CObjectManager instance = new CObjectManager();
+
+        private List<CGameObject> objectList;
+        
+        public CObjectManager()
+        {
+            objectList = null;
+        }
+
+        /// <summary>
+        /// Gets the instance of the ObjectManager
+        /// </summary>
+        public static CObjectManager Instance
+        {
+            get { return instance; }
+        }
+
+        /// <summary>
+        /// Initializes the ObjectManager
+        /// Creates a new instance of the object list
+        /// </summary>
+        public void Initialize()
+        {
+            objectList = new List<CGameObject>();
+        }
+
+        /// <summary>
+        /// Clears the object list
+        /// </summary>
+        public void Shutdown()
+        {
+            objectList.Clear();
+        }
+
+        public void Update(float fElapsedTime)
+        {
+            for (int i = 0; i < objectList.Count; ++i)
+            {
+                objectList[i].Update(fElapsedTime);
+            }
+        }
+
+        public void AddObject(CGameObject addObject)
+        {
+            if (addObject == null)
+                return;
+
+            addObject.ListID = objectList.Count;
+            objectList.Add(addObject);
+        }
+
+        public void RemoveObject(CGameObject removeObject)
+        {
+            objectList.Remove(removeObject);
+        }
+    }
+}
